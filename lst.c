824db81b7cda59bd79ff9800fe6e7ae17913c6a8
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2017-04-24 21:21:44 -0700

    lst.c fixed

--- lst.c
+++ lst.c
@@ -6,29 +6,39 @@
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 16:47:57 by vyudushk          #+#    #+#             */
-/*   Updated: 2017/04/11 19:13:15 by vyudushk         ###   ########.fr       */
+/*   Updated: 2017/04/24 21:17:12 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "libls.h"
+#include <unistd.h>
+#include <dirent.h>
+
+/*
+** I'm just keeping here so that I remember what I learned
+** from kdavis.
+**
+**	struct dirent	hold;
+**
+**	hold = *((struct dirent*)(lst->content));
+**	ft_putendl(hold.d_name);
+*/
 
 void		print_lst_dot(t_list *lst)
 {
-	ft_putendl(lst->content);
+	ft_putendl(((struct dirent*)(lst->content))->d_name);
 }
 
 void		print_lst(t_list *lst)
 {
-	char	*str;
-
-	str = lst->content;
-	if (str[0] != '.')
-		ft_putendl(lst->content);
+	if (((struct dirent*)(lst->content))->d_name[0] != '.')
+		ft_putendl(((struct dirent*)(lst->content))->d_name);
 }
 
 void		process_lst(t_list *lst, t_flag *options)
 {
+	sort_lst(&lst);
 	if (options->include_dot)
 		ft_lstiter(lst, print_lst_dot);
 	else
