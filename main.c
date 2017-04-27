3a79dd1acfe4e2a9e83526dba4b6e7f4b7ca157e
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2017-04-27 15:10:07 -0700

    figuring some stuff out yo

--- main.c
+++ main.c
@@ -6,14 +6,15 @@
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 13:34:41 by vyudushk          #+#    #+#             */
-/*   Updated: 2017/04/24 21:22:37 by vyudushk         ###   ########.fr       */
+/*   Updated: 2017/04/27 15:07:48 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libls.h"
 #include "libft.h"
-#include <dirent.h>
+#include <dirent.h> // DIR and dirent
 #include <stdio.h>
+#include <sys/stat.h> //stat()
 
 int	main(int argc, char **argv)
 {
@@ -21,6 +22,7 @@ int	main(int argc, char **argv)
 	struct dirent	*file;
 	t_list			*lst;
 	t_flag			*options;
+	struct stat		buf;
 
 	options = parse_flags(argc, argv);
 	new_dir = find_dir(argc, argv);
@@ -28,6 +30,9 @@ int	main(int argc, char **argv)
 	while ((file = readdir(new_dir)))
 	{
 		//ft_lstadd(&lst, ft_lstnew(file->d_name, ft_strlen(file->d_name)));
+		stat(file->d_name, &buf);
+		ft_putnbr(S_ISDIR(buf.st_mode));
+		ft_putchar('\n');
 		ft_lstadd(&lst, ft_lstnew(file, sizeof(struct dirent)));
 	}
 	process_lst(lst, options);
