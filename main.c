45490c3c65ca80336eb307449a7b2c635e648f6f
Vladislav Yudushkin <Vlad.Yudushkin@gmail.com>
2017-05-02 16:10:37 -0700

    kane told me to initilize my memory

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   copyofmain_deletemeltrpls.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vyudushk <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/28 13:34:41 by vyudushk          #+#    #+#             */
-/*   Updated: 2017/05/01 15:31:28 by vyudushk         ###   ########.fr       */
+/*   Updated: 2017/05/02 16:07:03 by vyudushk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,32 @@
 #include <sys/stat.h> //stat()
 #include <stdlib.h>
 
-int	main(int argc, char **argv)
+int	print_dir(t_flag *options, DIR *new_dir)
 {
-	DIR				*new_dir;
-	t_list			*lst;
-	t_flag			*options;
-	t_file			*file;
+	t_file	*file;
+	t_list	*lst;
 
-	options = parse_flags(argc, argv);
-	new_dir = find_dir(argc, argv);
-	valid_check(new_dir, argv[1]);
 	file = (t_file*)malloc(sizeof(t_file));
+	if (file == 0)
+		return (0);
+	lst = 0;
 	while ((file->name = readdir(new_dir)))
 	{
 		stat(file->name->d_name, file->info);
 		ft_lstadd(&lst, ft_lstnew(file, sizeof(file)));
 	}
 	process_lst(lst, options);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	DIR				*new_dir;
+	t_flag			*options;
+
+	options = parse_flags(argc, argv);
+	new_dir = find_dir(argc, argv);
+	valid_check(new_dir, argv[1]);
+	print_dir(options, new_dir);
 	return (0);
 }
